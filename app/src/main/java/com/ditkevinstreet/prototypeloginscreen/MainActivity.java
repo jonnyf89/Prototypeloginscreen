45419acc79a996d6175f41e755f718e3a12dcc01
jonnyf89 <jonathan.gorselodge@gmail.com>
2017-10-09 16:55:23 +0100

    Initial commit

---
+++ app/src/main/java/com/ditkevinstreet/prototypeloginscreen/MainActivity.java
@@ -0,0 +1,94 @@
+package com.ditkevinstreet.prototypeloginscreen;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // UI References
+    private EditText mEmail, mPassword;
+    private Button btnSubmit, btnSignOut;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //declare buttons and edit texts in onCreate
+        mEmail = (EditText) findViewById(R.id.Email);
+        mPassword = (EditText) findViewById(R.id.Password);
+        btnSubmit = (Button) findViewById(R.id.Submit);
+        btnSignOut = (Button) findViewById(R.id.SignOut);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) { //this if statement probably isnt necessary as the else code wont be used, this prototype only allows siging in
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if(!email.equals("")&& !pass.equals("")){
+                    mAuth.signInWithEmailAndPassword(email, pass);
+                }else{
+                    toastMessage("You didn't fill in all the fields");
+                }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessage("Signed out");
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void toastMessage(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+}
