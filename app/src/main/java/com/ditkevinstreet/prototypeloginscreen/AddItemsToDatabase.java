6f53803c61b50a0a1d14c7849ee2d728db01bb43
jonnyf89 <jonathan.gorselodge@gmail.com>
2017-10-21 18:47:10 +0100

    Updated the app to inlcude an 'add to database' screen

---
+++ app/src/main/java/com/ditkevinstreet/prototypeloginscreen/AddItemsToDatabase.java
@@ -0,0 +1,117 @@
+package com.ditkevinstreet.prototypeloginscreen;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by Admin on 18/10/2017.
+ */
+
+public class AddItemsToDatabase extends AppCompatActivity {
+
+    private static final String TAG = "AddItemsToDatabase";
+
+    private Button mAddToDB;
+    private EditText mNewItem;
+
+    //add Firebase DB stuff
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.additemstodatabase);
+
+        mAddToDB = (Button) findViewById(R.id.btnAddNewItem);
+        mNewItem = (EditText) findViewById(R.id.add_new_item);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) { //this if statement probably isnt necessary as the else code wont be used, this prototype only allows siging in
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Object value = dataSnapshot.getValue();
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        mAddToDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Attempting to add object to Database");
+                String newItem = mNewItem.getText().toString();
+                if(!newItem.equals("")){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String userID = user.getUid();
+                    myRef.child(userID).child("Username").child(newItem).setValue("true");
+                    toastMessage("Adding " + newItem + " to the database...");
+                    //reset the text
+                    mNewItem.setText("");
+                }
+            }
+        });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void toastMessage(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+}
